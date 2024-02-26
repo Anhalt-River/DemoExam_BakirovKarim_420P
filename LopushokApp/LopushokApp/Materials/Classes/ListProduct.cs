a53cea87f74fb5c36c05b80a13f55360e96ecd1d
Anhalt-River <85508867+Anhalt-River@users.noreply.github.com>
2024-02-26 17:38:41 +0300

    Задание модели в приложении, создание класса продукта и написание инструкции к установке

--- LopushokApp/LopushokApp/Materials/Classes/ListProduct.cs
+++ LopushokApp/LopushokApp/Materials/Classes/ListProduct.cs
@@ -8,6 +8,12 @@ namespace LopushokApp.Materials.Classes
 {
     public class ListProduct
     {
-
+        public int Articul { get; set; }           
+        public string NameProduct { get; set; }
+        public int IdTypeProduct { get; set; }
+        public string TypeProduct { get; set; }
+        public string Materials { get; set; }
+        public string Price { get; set; }
+        public string ImagePath { get; set; } = "/Materials/picture.png";
     }
 }
