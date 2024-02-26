ba6b6a8f4eb110672fc003c67baaf426a3e1db78
Anhalt-River <85508867+Anhalt-River@users.noreply.github.com>
2024-02-26 18:35:15 +0300

    Последний коммит. Фильтрация и поиск остались в коде, но не были приспособлены к интерфейсу. Поиск остался работать на уровне подсказки при наведении

--- LopushokApp/LopushokApp/Pages/ProductListPage.xaml.cs
+++ LopushokApp/LopushokApp/Pages/ProductListPage.xaml.cs
@@ -28,11 +28,11 @@ namespace LopushokApp.Pages
         public ProductListPage()
         {
             InitializeComponent();
-            BasicLoader();
+            ListRemake();
         }
 
         List<ListProduct> all_products;
-        private void BasicLoader()
+        private void ListRemake()
         {
             all_products = new List<ListProduct>();
 
@@ -110,6 +110,101 @@ namespace LopushokApp.Pages
             {
                 SearchBox.Text = "Введите для поиска";
             }
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            /*List<ListProduct> new_list = new List<ListProduct>();
+            foreach (var product in all_products)
+            {
+                if (product.NameProduct.Contains(SearchBox.Text))
+                {
+                    new_list.Add(product);
+                }
+            }
+
+            all_products.Clear();
+            all_products = new_list;
+            ProductsList.ItemsSource = null;
+            ProductsList.ItemsSource = all_products;*/
+        }
+
+        //Переработчик для фильтрации и поиска
+        private void ListRemake(string search_text)
+        {
+            all_products = new List<ListProduct>();
+
+            var row_products = App.Connection.Products.Where(x=> x.Name_Product.Contains(search_text)).ToList();
+            foreach (var row_prod in row_products)
+            {
+                ListProduct new_prod = new ListProduct();
+                new_prod.Articul = row_prod.Articul_Product;
+                new_prod.NameProduct = row_prod.Name_Product;
+
+                if (row_prod.Id_TypeProduct != null)
+                {
+                    new_prod.IdTypeProduct = row_prod.Id_TypeProduct;
+                    var TypeProduct = App.Connection.Type_Product.
+                        Where(x => x.Id_TypeProduct == new_prod.IdTypeProduct).
+                        FirstOrDefault();
+                    new_prod.TypeProduct = TypeProduct.Name_TypeProduct;
+                }
+
+                var search_allMaterials = App.Connection.ProductMaterial.
+                    Where(x => x.Articul_Product == new_prod.Articul).ToList();
+                double? final_price = 0;
+                foreach (var material in search_allMaterials)
+                {
+                    int? count_materials = material.Count_Material;
+                    if (count_materials != null && count_materials != 0)
+                    {
+                        var search_material = App.Connection.Materials.
+                            Where(x => x.Id_Material == material.Id_Material).
+                            FirstOrDefault();
+                        new_prod.Materials += $"{search_material.Name_Material}, ";
+
+                        double? price_for_one = search_material.Price;
+
+                        if (price_for_one != null && price_for_one != 0)
+                        {
+                            final_price += price_for_one * count_materials;
+                        }
+                    }
+                }
+                new_prod.Price = final_price.ToString();
+
+                if (row_prod.Image != "нет")
+                {
+                    string take_path = row_prod.Image;
+                    new_prod.ImagePath = $"/Materials/Products/{take_path}";
+                }
+
+                new_prod.Image = new BitmapImage(new Uri($"pack://application:,,,{new_prod.ImagePath}"));
+
+
+
+                all_products.Add(new_prod);
+            }
+
+            ProductsList.ItemsSource = all_products;
+        }
+
+        private void SearchBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (SearchBox.Text.Length == 0)
+            {
+                SearchBox.Text = "Введите для поиска";
+                return;
+            }
+
+            if (SearchBox.Text == "Введите для поиска")
+            {
+                SearchBox.Text = "";
+                return;
+            }
+
+            ListRemake(SearchBox.Text);
         }
     }
 }
