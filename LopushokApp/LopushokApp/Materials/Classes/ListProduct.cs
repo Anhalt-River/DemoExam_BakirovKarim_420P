625312cb3b823d5559d7b28dcb1a975ad1c567e3
Anhalt-River <85508867+Anhalt-River@users.noreply.github.com>
2024-02-26 17:58:28 +0300

    Переработка элемента таблицы базы данных, предварительное окончание работы кода над списком

--- LopushokApp/LopushokApp/Materials/Classes/ListProduct.cs
+++ LopushokApp/LopushokApp/Materials/Classes/ListProduct.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LopushokApp.Materials.Classes
+﻿namespace LopushokApp.Materials.Classes
 {
     public class ListProduct
     {
         public int Articul { get; set; }           
         public string NameProduct { get; set; }
-        public int IdTypeProduct { get; set; }
+        public int? IdTypeProduct { get; set; }
         public string TypeProduct { get; set; }
-        public string Materials { get; set; }
+        public string Materials { get; set; } = "Материалы:";
         public string Price { get; set; }
         public string ImagePath { get; set; } = "/Materials/picture.png";
     }
--- LopushokApp/LopushokApp/Pages/ProductListPage.xaml.cs
+++ LopushokApp/LopushokApp/Pages/ProductListPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LopushokApp.AppData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using LopushokApp.Materials.Classes;
+using System.Windows.Media.Media3D;
+
 namespace LopushokApp.Pages
 {
     /// <summary>
@@ -26,9 +30,57 @@ namespace LopushokApp.Pages
             BasicLoader();
         }
 
+        List<ListProduct> all_products;
         private void BasicLoader()
         {
+            var row_products = App.Connection.Products.ToList();
+            foreach (var row_prod in row_products)
+            {
+                ListProduct new_prod = new ListProduct();
+                new_prod.Articul = row_prod.Articul_Product;
+                new_prod.NameProduct = row_prod.Name_Product;
 
+                if (row_prod.Id_TypeProduct != null)
+                {
+                    new_prod.IdTypeProduct = row_prod.Id_TypeProduct;
+                    var TypeProduct = App.Connection.Type_Product.
+                        Where(x=> x.Id_TypeProduct == new_prod.IdTypeProduct).
+                        FirstOrDefault();
+                    new_prod.TypeProduct = TypeProduct.Name_TypeProduct;
+                }
+
+                var search_allMaterials = App.Connection.ProductMaterial.
+                    Where(x=> x.Articul_Product == new_prod.Articul).ToList();
+                double? final_price = 0;
+                foreach (var material in search_allMaterials)
+                {
+                    int? count_materials = material.Count_Material;
+                    if (count_materials != null && count_materials != 0)
+                    {
+                        var search_material = App.Connection.Materials.
+                            Where(x=> x.Id_Material == material.Id_Material).
+                            FirstOrDefault();
+                        new_prod.Materials += $"{search_material.Name_Material}, ";
+
+                        double? price_for_one = search_material.Price;
+
+                        if (price_for_one != null && price_for_one != 0)
+                        {
+                            final_price += price_for_one * count_materials;
+                        }
+                    }
+                }
+
+                if (row_prod.Image != "нет")
+                {
+                    string take_path = row_prod.Image;
+                    new_prod.ImagePath = $"/Materials{take_path}";
+                }
+
+                all_products.Add(new_prod);
+            }
+
+            ProductsList.ItemsSource = all_products;
         }
 
         private void SearchBox_MouseEnter(object sender, MouseEventArgs e)
